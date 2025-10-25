5a0f57cee89251c5b0790c873a9704d0ea038657
joshuaherdiman <joshuaherdiman@gmail.com>
2025-10-25 09:33:37 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from fastai.vision.all import *
+from requirements.txt import *
+
+st.title("Number Identifier")
+st.text("Built By Joshua")
+
+number_classification_model = load_learner("10-AM-Single-Number-Identification-(1).pkl")
+
+def number_identifier(file_name):
+    file_parts = str(file_name).split("/")
+    folder_name = file_parts[-2]
+    return folder_name
+
+def predict(image):
+    img = PILImage.creat(image)
+    pred_class, pred_idx, outputs = number_classification_model.predict(img)
+    return pred_class
+
+uploaded_file = st.file_uploader("Choose an image to upload...", type=["jpg", "png", "jpeg"])
+
+if uploaded_file is not None:
+    st.image(uploaded_file, caption="Uploaded Image", use_container_width=True)
+    prediction = predict(uploaded_file)
+    st.subheader(f"Predicted Breed: {prediction}")
+
+st.text("Built with Streamlit and FastAI.")
